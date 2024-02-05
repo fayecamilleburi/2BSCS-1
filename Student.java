b8c29d02adb57bcb62eb833d2f74e569211bfe19
Faye Camille Buri <152823567+fayecamilleburi@users.noreply.github.com>
2024-02-05 04:27:25 +0000

    New

---
+++ Student.java
@@ -0,0 +1,32 @@
+public class Student {
+    private String studentNo;
+    private String lastName;
+    private String firstName;
+
+    //setter method
+
+    public void setStudentNo(String studentNo) {
+        this.studentNo = studentNo;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    //getter method
+    public String getStudentNo() {
+        return studentNo;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+}
